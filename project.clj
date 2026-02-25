00d8ebdb43edaabc3009f5abcd6c368dd249bc37
Jonathan Newman <jonathan.newman@puppetlabs.com>
2026-02-25 08:53:25 -0800

    (maint) Update parent project version to 7.3.57
    This resolves multiple CVEs being reported by Mend for the repo.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "7.3.5"]
+  :parent-project {:coords [puppetlabs/clj-parent "7.3.57"]
                    :inherit [:managed-dependencies]}
 
   ;; Abort when version ranges or version conflicts are detected in
